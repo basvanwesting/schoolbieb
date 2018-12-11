89bc08639e2cb279b12e896f718dc9d104ea7480
basvanwesting <basvanwesting@hotmail.com>
2018-12-11 21:52:19 +0100

    redo reset and author initialization

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,8 +1,9 @@
 class Author < ApplicationRecord
   has_many :books
 
-  validates :first_name, presence: true
+  #validates :first_name, presence: true
   validates :last_name,  presence: true
+  validates :first_name, uniqueness: { scope: [:last_name, :middle_name] }
 
   def full_name
     [
--- doc/beheer/reset.rb
+++ doc/beheer/reset.rb
@@ -5,15 +5,18 @@ middle_regexp = /\s[a-z]+(\s[a-z]+)?/
 last_regexp = /\s[A-Za-z]+$/
 File.open('doc/authors.txt').each_line do |line|
   line.strip!
-  middle = line[middle_regexp].to_s
-  line.gsub!(middle, '') if middle.present?
-  last = line[last_regexp].to_s
-  first = line.gsub(last, '').to_s if last.present?
-  first ||= line.to_s
+  last_name, first_and_middle_name = line.split(',',2)
+  last_name = last_name.to_s.strip
+  first_and_middle_name = first_and_middle_name.to_s.strip
 
-  first_name  = first.strip
-  middle_name = middle.strip
-  last_name   = last.strip
+  middle_name = first_and_middle_name[middle_regexp].to_s
+
+  first_and_middle_name.gsub!(middle_name, '') if middle_name.present?
+  first_name = first_and_middle_name
+
+  first_name  = first_name.strip.capitalize
+  middle_name = middle_name.strip.downcase
+  last_name   = last_name.strip.capitalize
 
   if first_name.present? && last_name.present?
     Author.create(
