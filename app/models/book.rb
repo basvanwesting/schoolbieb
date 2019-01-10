e6ee37253968e81cad7adfd6663419a430814db9
basvanwesting <basvanwesting@hotmail.com>
2019-01-10 19:54:51 +0100

    Add enrich extended_title for books without extended_title

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,6 +3,8 @@ class Book < ApplicationRecord
 
   validates :title, presence: true
 
+  before_save :extend_title!
+
   module ReadingLevels
     All = %w[ A B C P ML ]
   end
@@ -26,6 +28,25 @@ class Book < ApplicationRecord
 
   def sanitized_title
     title.sub(/\[\[.*?\]\]/,'')
+  end
+
+  def extended_title?
+    title.to_s[/\[\[.*\]\]$/]
+  end
+
+  def extended_title
+    return title if extended_title?
+    enrichment = [
+      reading_level.present? ? reading_level  : nil,
+      avi_level.present?     ? avi_level      : nil,
+      part.present?          ? "deel #{part}" : nil,
+    ].compact.join(' | ')
+    "#{title} [[#{enrichment}]]"
+  end
+
+  def extend_title!
+    return if extended_title?
+    self.title = extended_title
   end
 
   def sticker
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,4 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
+
+  describe 'extended_title' do
+    subject { FactoryBot.build(:book) }
+
+    it 'returns the title if already extended' do
+      subject.title = "My first book [[A | B | C]]"
+      subject.reading_level = "P"
+      subject.avi_level = "M4"
+      expect(subject.extended_title).to eq 'My first book [[A | B | C]]'
+    end
+
+    it 'extends title if not extended yet' do
+      subject.reading_level = "PL"
+      subject.avi_level = "M3"
+      expect(subject.extended_title).to eq 'My First Book [[PL | M3]]'
+    end
+
+    it 'extends title if not extended yet, but noting to extend' do
+      expect(subject.extended_title).to eq 'My First Book [[]]'
+    end
+
+    it 'persists the extended title if not extended yet' do
+      subject.reading_level = "PL"
+      subject.avi_level = "M3"
+      expect(subject.title).to eq 'My First Book'
+      subject.save && subject.reload
+      expect(subject.title).to eq 'My First Book [[PL | M3]]'
+    end
+
+  end
 end
