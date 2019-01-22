3aa37ac365f2c3bc5ae9d28d888838bf3daab16a
basvanwesting <basvanwesting@hotmail.com>
2019-01-22 22:05:38 +0100

    update search counts for pagination

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def search_result_info(records)
+    from_item = (records.current_page - 1) * records.limit_value + 1
+    to_item = [from_item + records.size - 1, records.total_count].min
+    from_item = 0 if records.total_count.zero?
+    "#{from_item} - #{to_item} / #{records.total_count}"
+  end
 end
