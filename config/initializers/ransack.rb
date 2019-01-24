203110b6721b221da11c1fda172daf3e6519c9e5
basvanwesting <basvanwesting@hotmail.com>
2019-01-24 10:08:46 +0100

    Add eq_any_split_whitespace for Book IDs in Sticker Index

--- config/initializers/ransack.rb
+++ config/initializers/ransack.rb
@@ -1,4 +1,12 @@
 Ransack.configure do |config|
+  config.add_predicate 'eq_any_split_whitespace', # Name your predicate
+    arel_predicate: 'in',
+    formatter: proc { |v| v.to_s.split(/\s+/).select(&:present?) },
+    validator: proc { |v| v.present? },
+    compounds: false,
+    wants_array: false,
+    type: :string
+
   config.add_predicate 'wildcard', # Name your predicate
     arel_predicate: 'in',
     formatter: proc { |v| Book.wildcard_search(v) },
