3e946e1e51b3274fec7bf1699c35bb6e2a69e8f1
basvanwesting <basvanwesting@hotmail.com>
2019-03-13 09:15:38 +0100

    Change GUI paging to 60 stickers per page Change Print paging to 6 stickers per page

--- app/controllers/stickers_controller.rb
+++ app/controllers/stickers_controller.rb
@@ -2,7 +2,7 @@ class StickersController < ApplicationController
 
   def index
     @q = Book.ransack(params[:q])
-    @books = @q.result.includes(:author).order("authors.last_name ASC, title ASC").page(params[:page]).per(70)
+    @books = @q.result.includes(:author).order("authors.last_name ASC, title ASC").page(params[:page]).per(60)
   end
 
 end
