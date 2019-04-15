ddbca18fa4fd6a7b429ac72958611ee66aa9dec7
basvanwesting <basvanwesting@hotmail.com>
2019-04-15 21:02:14 +0200

    update data-controller book-form

--- app/javascript/controllers/book_form_controller.js
+++ app/javascript/controllers/book_form_controller.js
@@ -1,9 +1,39 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ "output" ]
+  static targets = [ "title", "titleList" ]
 
-  connect() {
-    this.outputTarget.textContent = 'Hello, Stimulus!'
+  inputTitle() {
+    if (this.titleTarget.value.length > 3) {
+      this.updateTitleOptions()
+    } else {
+      this.clearTitleOptions()
+    }
+  }
+
+  clearTitleOptions() {
+    while (this.titleListTarget.hasChildNodes()) {
+      this.titleListTarget.removeChild(this.titleListTarget.firstChild)
+    }
+  }
+
+  setTitleOptions(values) {
+    this.clearTitleOptions()
+
+    values.forEach(v => {
+      const opt = document.createElement("option")
+      opt.value = v
+      this.titleListTarget.appendChild(opt)
+    })
+  }
+
+  updateTitleOptions() {
+    fetch(
+      `/titles?term=${this.titleTarget.value}`,
+      { headers: { "Content-Type": "application/json; charset=utf-8", "Accept": "application/json" } },
+    )
+      .then(res => res.json())
+      .then(response => this.setTitleOptions(response))
+      .catch(err => console.log(err))
   }
 }
