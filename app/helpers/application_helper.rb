b5412d3ee4e01e428245659800d66827d521a371
basvanwesting <basvanwesting@hotmail.com>
2020-06-08 15:47:54 +0200

    update inline_search_form etc.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,14 @@
 module ApplicationHelper
+  def inline_search_form_for(*args, **options, &block)
+    merged_options = options.merge(wrapper: :inline_search_form)
+    search_form_for(*args, merged_options, &block)
+  end
+
+  def search_row(&blk)
+    content = capture(&blk)
+    content_tag(:tr, content,  class: ['search', @q.conditions.present? ? 'conditions-present' : ''] )
+  end
+
   def search_result_info(records)
     from_item = (records.current_page - 1) * records.limit_value + 1
     to_item = [from_item + records.size - 1, records.total_count].min
