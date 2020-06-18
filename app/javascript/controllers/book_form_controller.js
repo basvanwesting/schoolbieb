c37ac4f39aadba934b932175463aaf5b71d9ffab
basvanwesting <basvanwesting@hotmail.com>
2020-06-18 16:43:48 +0200

    remove book_title_mixin

--- app/javascript/controllers/book_form_controller.js
+++ app/javascript/controllers/book_form_controller.js
@@ -2,8 +2,6 @@ import { Controller } from "stimulus"
 import consumer from "../channels/consumer"
 import { debounce } from "lodash"
 
-import bookTitleMixin from "./book_title_mixin"
-
 const RESULT_LIMIT = 25
 export default class BookFormController extends Controller {
 
@@ -62,50 +60,50 @@ export default class BookFormController extends Controller {
     )
   }
 
-//  ///////////////////// Book#title //////////////////
-//
-//  inputTitle() {
-//    if (this.titleTarget.value.length > 3) {
-//      this.updateTitleOptions()
-//    } else {
-//      this.clearTitleOptions()
-//    }
-//  }
-//
+  ///////////////////// Book#title //////////////////
+
+  inputTitle() {
+    if (this.titleTarget.value.length > 3) {
+      this.updateTitleOptions()
+    } else {
+      this.clearTitleOptions()
+    }
+  }
+
   debouncedInputTitle = debounce(this.inputTitle, 300)
-//
-//  clearTitleOptions() {
-//    while (this.titleListTarget.hasChildNodes()) {
-//      this.titleListTarget.removeChild(this.titleListTarget.firstChild)
-//    }
-//  }
-//
-//  setTitleOptions(titles) {
-//    if (titles.length === 1 && titles[0] === this.titleTarget.value) {
-//      this.clearTitleOptions()
-//    } else if (titles.length >= RESULT_LIMIT) {
-//      const opt = document.createElement("option")
-//      opt.value = `${this.titleTarget.value}... Too many results (${titles.length}), type more`
-//      $(this.titleListTarget)
-//        .empty()
-//        .append(opt)
-//    } else {
-//      const newOptions = titles.map(v => {
-//        const opt = document.createElement("option")
-//        opt.value = v
-//        return opt
-//      })
-//      $(this.titleListTarget)
-//        .empty()
-//        .append(newOptions)
-//    }
-//  }
-//
-//  updateTitleOptions() {
-//    this.autocompleteChannel.search_book_title({ title_cont: this.titleTarget.value })
-//  }
-//
-//  ///////////////////// Book#series //////////////////
+
+  clearTitleOptions() {
+    while (this.titleListTarget.hasChildNodes()) {
+      this.titleListTarget.removeChild(this.titleListTarget.firstChild)
+    }
+  }
+
+  setTitleOptions(titles) {
+    if (titles.length === 1 && titles[0] === this.titleTarget.value) {
+      this.clearTitleOptions()
+    } else if (titles.length >= RESULT_LIMIT) {
+      const opt = document.createElement("option")
+      opt.value = `${this.titleTarget.value}... Too many results (${titles.length}), type more`
+      $(this.titleListTarget)
+        .empty()
+        .append(opt)
+    } else {
+      const newOptions = titles.map(v => {
+        const opt = document.createElement("option")
+        opt.value = v
+        return opt
+      })
+      $(this.titleListTarget)
+        .empty()
+        .append(newOptions)
+    }
+  }
+
+  updateTitleOptions() {
+    this.autocompleteChannel.search_book_title({ title_cont: this.titleTarget.value })
+  }
+
+  ///////////////////// Book#series //////////////////
 
   inputSeries() {
     if (this.seriesTarget.value.length > 2) {
@@ -196,5 +194,3 @@ export default class BookFormController extends Controller {
     this.autocompleteChannel.search_author({ id_author_wildcard: this.authorDescriptionTarget.value })
   }
 }
-
-Object.assign(BookFormController.prototype, bookTitleMixin);
