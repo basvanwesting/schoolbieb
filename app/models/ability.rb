33821943b2413a6b3cf5f04eff30ed3ffd6f6549
basvanwesting <basvanwesting@hotmail.com>
2020-06-19 20:23:10 +0200

    Add can? checks to views

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -9,15 +9,19 @@ class Ability
     end
 
     if user.role?(Role::JuniorCollaborator)
+      can :read, Author
       can :read, Book
       can :manage, Lending::Borrow
       can :manage, Lending::Return
     end
 
     if user.role?(Role::SeniorCollaborator)
+      can :manage, Author
       can :manage, Book
       can :manage, Lending::Borrow
       can :manage, Lending::Return
+      can :read, Lender
+      can :read, Loan
     end
   end
 end
