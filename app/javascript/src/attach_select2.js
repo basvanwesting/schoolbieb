865438ccd9fbae8f241da0756ef74fd1cf7731df
basvanwesting <basvanwesting@hotmail.com>
2020-07-14 16:16:42 +0200

    remove support for remaining select2, except for js-select2-free-tags

--- app/javascript/src/attach_select2.js
+++ app/javascript/src/attach_select2.js
@@ -1,29 +1,5 @@
 export default class AttachSelect2 {
   static init() {
-    $('select.js-select2').each(function(_index) {
-      const el = $(this)
-      const options = {
-        theme: 'foundation',
-        allowClear: true,
-        placeholder: ""
-      }
-      // console.log(`init select2 ${el}`)
-      el.select2(options)
-    })
-
-    $("select.js-select2-ajax").select2({
-      theme: "foundation",
-      allowClear: true,
-      ajax: {
-        dataType: "json",
-        processResults: function (data) {
-          return {
-            results: data.map(function (item) { return { id: item, text: item } } )
-          }
-        }
-      }
-    })
-
     $("select.js-select2-free-tags").select2({
       theme: "foundation",
       //allowClear: true,
