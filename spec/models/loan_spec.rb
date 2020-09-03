9b5318ed536e309f1f94f8ec9743b1627e52a4ab
basvanwesting <basvanwesting@hotmail.com>
2020-09-03 19:40:06 +0200

    handle whitespace differences in paper_trail yaml in tests

--- spec/models/loan_spec.rb
+++ spec/models/loan_spec.rb
@@ -14,11 +14,11 @@ RSpec.describe Loan, type: :model do
       BookUseCase::Prolong.new(book_id: book.id).save
       BookUseCase::Return.new(book_id: book.id).save
 
-      expect(Loan.last.versions.pluck(:event, :object_changes)).to match [
+      expect(Loan.last.versions.pluck(:event, :object_changes).flatten.map(&:squish)).to match [
         ["create", "---\nlending_date:\n- \n- 2020-01-01\ndue_date:\n- \n- 2020-01-22\n"],
         ["update", "---\ndue_date:\n- 2020-01-22\n- 2020-02-12\ntimes_prolonged:\n- 0\n- 1\n"],
         ["update", "---\nreturn_date:\n- \n- 2020-01-01\n"],
-      ]
+      ].flatten.map(&:squish)
     end
   end
 
